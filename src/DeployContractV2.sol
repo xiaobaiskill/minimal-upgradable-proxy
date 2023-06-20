e2ccc04b76379fd1376f608aaad86740eb128e42
jmz <1125378902@qq.com>
2023-06-20 16:45:48 +0800

    v2

--- src/DeployContractV2.sol
+++ src/DeployContractV2.sol
@@ -6,9 +6,6 @@ interface implement {
 }
 
 contract DeployContractV2 {
-    bytes constant proxyCode =
-        hex"602080604038033d39808038038139513d51553d51603a80602460403990604a526040f3363d3d373d3d3d363d7f0000000000000000000000000000000000000000000000000000000000000000545af43d82803e3d8282603857fd5bf3";
-
     function createContract(address _logic) external returns (address proxy) {
         bytes memory code = abi.encodePacked(
             hex"7f",
